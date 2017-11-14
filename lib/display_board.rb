1cbc044b629eb694113a1f698e3c5d69050f3906
Phoebe Lau <phoebelau7@gmail.com>
2017-11-14 03:00:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+player_move = ["X", "O"]
 def display_board (board)
-  puts "   |   |   "
+  puts " #{player_move} | #{player_move} | #{player_move} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{player_move} | #{player_move} | #{player_move} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{player_move} | #{player_move} | #{player_move} "
+
+
 end
