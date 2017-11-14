9808b331b46981dc7f49c900c07928217b7de492
Phoebe Lau <phoebelau7@gmail.com>
2017-11-14 02:55:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
+def board
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+end
+
+def display_board (board)
+  
 end
